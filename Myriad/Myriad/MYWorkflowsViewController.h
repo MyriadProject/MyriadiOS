8bd0403f534e1ab71d733e98176f316d8077f55e
Ethan Gill <ethan.gill@me.com>
2014-05-03 11:20:55 -0500

    added template code for the Table View and Collection View

---
+++ Myriad/Myriad/MYWorkflowsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MYWorkflowsViewController.h
+//  Myriad
+//
+//  Created by Ethan Gill on 5/3/14.
+//  Copyright (c) 2014 Myriad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MYWorkflowsViewController : UICollectionViewController
+
+@end
