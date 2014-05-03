512730cc063f98cac9681f77b9a585ee286ca847
Ethan Gill <ethan.gill@me.com>
2014-05-03 15:01:02 -0500

    added test view controller to tab bar

---
+++ Myriad/Myriad/MYTestViewController.m
@@ -0,0 +1,52 @@
+//
+//  MYTestViewController.m
+//  Myriad
+//
+//  Created by Ethan Gill on 5/3/14.
+//  Copyright (c) 2014 Myriad. All rights reserved.
+//
+
+#import "MYTestViewController.h"
+
+@interface MYTestViewController ()
+
+@end
+
+@implementation MYTestViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+- (IBAction)sendTestCommand:(id)sender {
+    
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
