0eea8f4e0f4fbe719b4bf583439b553b09fe392e
John Saba <john.thomas.saba@gmail.com>
2014-05-03 15:11:23 -0500

    add test device

---
+++ Myriad/Myriad/MYDevice.h
@@ -0,0 +1,18 @@
+//
+//  MYDevice.h
+//  Myriad
+//
+//  Created by John Saba on 5/3/14.
+//  Copyright (c) 2014 Myriad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MYDevice : NSObject
+
+@property (copy, nonatomic) NSString *name;
+@property (strong, nonatomic) NSArray *commands;
+
+- (instancetype)initWithJson:(NSDictionary *)json;
+
+@end
