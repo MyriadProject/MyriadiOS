66cd4098702ac79460c9c87171fa32c8adc20144
John Saba <john.thomas.saba@gmail.com>
2014-05-04 14:29:45 -0500

    commands collection

---
+++ Myriad/Myriad/MYCommandCell.m
@@ -0,0 +1,23 @@
+//
+//  MYCommandCell.m
+//  Myriad
+//
+//  Created by John Saba on 5/4/14.
+//  Copyright (c) 2014 Myriad. All rights reserved.
+//
+
+#import "MYCommandCell.h"
+
+
+@implementation MYCommandCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+@end
